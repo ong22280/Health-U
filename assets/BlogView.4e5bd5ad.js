03058335b904eaac90ee6e2a80c36dbbf2b4a45a
Sittipong Hemloun <77152671+ong22280@users.noreply.github.com>
2022-11-19 18:37:39 +0700

    first(2) deploy

---
+++ assets/BlogView.4e5bd5ad.js
@@ -0,0 +1,1 @@
+import{_ as o,N as a,r as n,o as t,a as r,f as s}from"./index.575e1923.js";const c={components:{Navbar:a}};function p(_,l,f,m,d,i){const e=n("Navbar");return t(),r("div",null,[s(e)])}const v=o(c,[["render",p]]);export{v as default};
